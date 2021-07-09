069e3ef7db013e8f46b242bfc67c36873709f686
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-09 16:40:33 -0700

    Add Players context

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -5,21 +5,33 @@ require './lib/player'
 describe Team do
   team = Team.new("France")
 
-  it 'exists' do
-    expect(team).to be_a(Team)
+  context 'Attributes' do
+    it 'exists' do
+      expect(team).to be_a(Team)
+    end
+
+    it 'has a country' do
+      expect(team.country).to eq("France")
+    end
+
+    it 'is not eliminated' do
+      expect(team.eliminated?).to eq(false)
+    end
+
+    it 'can be eliminated' do
+      team.eliminated
+
+      expect(team.eliminated?).to eq(true)
+    end
   end
 
-  it 'has a country' do
-    expect(team.country).to eq("France")
-  end
+  context 'Players' do
+    mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
 
-  it 'is not eliminated' do
-    expect(team.eliminated?).to eq(false)
-  end
-
-  it 'can be eliminated' do
-    team.eliminated
-
-    expect(team.eliminated?).to eq(true)
+    it 'exists' do
+      expect(mbappe).to be_a(Player)
+      expect(pogba).to be_a(Player)
+    end
   end
 end
