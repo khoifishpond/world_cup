14b6c5aa94811c8d7353488a62d267d1f85e2a00
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-09 19:16:12 -0700

    Update Player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,8 @@
 class Player
-  attr_reader :name, :position#, :hash
+  attr_reader :name, :position
 
-  def initialize(hash)
-    # @hash = hash  # is this necessary?
-    @name = hash[:name]
-    @position = hash[:position]
+  def initialize(player_params)
+    @name = player_params[:name]
+    @position = player_params[:position]
   end
 end
