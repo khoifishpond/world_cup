211adbc17bb85abaad11f53371dd3913d0aad240
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-09 19:16:36 -0700

    add active_players_by_category method

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -1,8 +1,14 @@
 class WorldCup
   attr_reader :year, :teams
-  
+
   def initialize(year, teams)
     @year = year
     @teams = teams  # array of two teams
+  end
+
+  def active_players_by_position(position)
+    @teams.filter_map do |team|
+      team.players_by_position(position) if team.eliminated? == false     
+    end.flatten
   end
 end
